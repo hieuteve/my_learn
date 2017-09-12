4b68eb990852f8047b8a5e6fee4c64e0638d1526
hieuteve <31719881+hieuteve@users.noreply.github.com>
2017-09-12 09:58:06 +0700

    Create AddExerciseHandler.java

---
+++ AddExerciseHandler.java
@@ -0,0 +1,23 @@
+package johnnyhieu.com.models;
+
+
+public class AddExercise2NewPinReqMsg extends BaseServiceReqMsg {
+	String pinName = "";
+	int exerciseId ;
+	
+	public void setPinName(String pinName) {
+		this.pinName = pinName;
+	}
+	
+	public String getPinName() {
+		return pinName;
+	}
+	
+	public void setExerciseId(int exerciseId) {
+		this.exerciseId = exerciseId;
+	}
+	
+	public int getExerciseId() {
+		return exerciseId;
+	}
+}
